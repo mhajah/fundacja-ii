2f02e8e007729b16173a89f76dee6a4a02e7e708
Michał Hajahmadov <331496@uwr.edu.pl>
2023-11-17 13:38:03 +0100

    initial init

---
+++ src/components/hero-image.vue
@@ -0,0 +1,37 @@
+<script setup>
+import navbar from './navbar.vue';
+</script>
+
+<template>
+    <navbar />
+    <section id="hero-main">
+        <h1 class="hero-main-header"><span>Fundacja Absolwentów Informatyki</span> Uniwersytetu Wrocławskiego</h1>
+
+    </section>
+</template>
+
+<style scoped>
+#hero-main {
+    height: 100vh;
+    background-image: linear-gradient(rgba(0,0,0,0.8), rgba(0,0,0,0.8)) ,url("/instytut.jpg");
+    background-position: 39% 50%;
+    background-repeat: no-repeat;
+    background-size: cover;
+    position: relative;
+    display: flex;
+    color: white;
+}
+
+.hero-main-header {
+    font-size: 24px;
+    width: 100%;
+    margin-top: 20vh;
+    text-align: center;
+    font-weight: 600;
+}
+
+span {
+    font-size: 24px;;
+}
+
+</style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,25 @@
+<template>
+    <section class="navbar-main">
+        <div class="navbar-main-logo">
+            LOGO
+        </div>
+    </section>
+</template>
+
+<style scoped>
+.navbar-main {
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 8vh;
+    width: 100%;
+    z-index: 99;
+    background-color: black;
+    color: white;
+    display: flex;
+    align-items: center;
+}
+.navbar-main-logo {
+    margin-left: 15px;
+}
+</style>
