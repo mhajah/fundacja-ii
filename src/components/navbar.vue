48677b041aa29d157d912dc8aaebdc2e24359559
mhajah <m.hajahmadov@gmail.com>
2023-11-27 19:37:56 +0100

    dekstop nav

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -61,7 +61,49 @@ import burgerMenu from './burgerMenu.vue'
         margin-right: 15px;
     }
     .list-menu-el {
-        margin-left: 15px;
+        margin-left: 30px;
+
+    }
+    .navbar-main-logo::after {
+        content: '';
+    display: block;
+    width: 0;
+    padding-bottom: 20px;
+    height: 1px;
+    border-bottom: 1px solid red;
+    transition: all .3s;
+    }
+
+    .list-menu-el::after {
+    content: '';
+    display: block;
+    width: 0;
+    padding-bottom: 20px;
+    height: 1px;
+    border-bottom: 1px solid red;
+    transition: all .3s;
+    }
+
+    .list-menu-el:hover::after {
+        width: 100%;
+        border-bottom: 1px solid red;
+        transition: all .3s;
+    }
+
+    /* .list-menu-el:hover {
+        transition: 0.5s;
+        box-shadow: inset 0 -30px 0 0 cyan; 
+        color: #B60D18;
+    } */
+
+    .navbar-main {
+        /*background: rgb(2,0,36);*/
+        /*background: linear-gradient(0deg, rgba(2,0,36,0) 0%, rgba(0,0,0,0.7469188358937324) 100%);*/
+        position: absolute;
+        background: inherit;
+        text-shadow: #000 2px 0 20px;
+        justify-content: space-around;
+        font-size: 20px;
     }
 }
 </style>
